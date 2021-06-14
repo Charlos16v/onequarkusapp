fcc5a646b1213881787a847f26aaae118a12ee69
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 18:01:38 +0200

    feat(resourcesolli): implemented getUsuaria endpoint GET method at "/usuaria/{nombre}" endpoint

--- src/main/java/edu/pingpong/onequarkusapp/resource/ResourcesOlli.java
+++ src/main/java/edu/pingpong/onequarkusapp/resource/ResourcesOlli.java
@@ -1,14 +1,13 @@
 package edu.pingpong.onequarkusapp.resource;
 
+import edu.pingpong.onequarkusapp.entity.Usuaria;
 import edu.pingpong.onequarkusapp.service.ServiceOlli;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/")
 public class ResourcesOlli {
@@ -25,5 +24,14 @@ public class ResourcesOlli {
     // curl -w "\n" http://localhost:8080/wellcome/
     public Response hello() {
         return Response.ok("Wellcome Ollivanders!").build();
+    }
+
+    @GET
+    @Path("/usuaria/{nombre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUsuaria(@PathParam("nombre") String usuariaNombre) {
+        Usuaria usuaria = service.cargaUsuaria(usuariaNombre);
+        return !usuaria.getNombre().equals("") ? Response.ok(usuaria).build() :
+                Response.status(Response.Status.NOT_FOUND).build();
     }
 }
