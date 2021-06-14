e574245d61ab99a2027f33803c0cd3cf9335b074
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 16:37:13 +0200

    feat(ServiceOlli): implemented ServiceOlli class the service layer

---
+++ src/main/java/edu/pingpong/onequarkusapp/service/ServiceOlli.java
@@ -0,0 +1,16 @@
+package edu.pingpong.onequarkusapp.service;
+
+import edu.pingpong.onequarkusapp.repository.RepositoryUsuaria;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+// Single bean instance
+@ApplicationScoped
+public class ServiceOlli {
+
+
+    public ServiceOlli() {}
+
+
+}
