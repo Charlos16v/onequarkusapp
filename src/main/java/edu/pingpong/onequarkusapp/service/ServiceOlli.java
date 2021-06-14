a335a42587b234e82bffd31a660d83abae9dbc8d
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 16:52:45 +0200

    feat(serviceolli): implemented cargaItem method encharged of return a Item if it exists if not return a Item with default values

--- src/main/java/edu/pingpong/onequarkusapp/service/ServiceOlli.java
+++ src/main/java/edu/pingpong/onequarkusapp/service/ServiceOlli.java
@@ -1,6 +1,8 @@
 package edu.pingpong.onequarkusapp.service;
 
+import edu.pingpong.onequarkusapp.entity.Item;
 import edu.pingpong.onequarkusapp.entity.Usuaria;
+import edu.pingpong.onequarkusapp.repository.RepositoryItem;
 import edu.pingpong.onequarkusapp.repository.RepositoryUsuaria;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,6 +16,9 @@ public class ServiceOlli {
     @Inject
     RepositoryUsuaria repositoryUsuaria;
 
+    @Inject
+    RepositoryItem repositoryItem;
+
     public ServiceOlli() {}
 
 
@@ -21,5 +26,11 @@ public class ServiceOlli {
         Optional<Usuaria> usuaria = this.repositoryUsuaria.getUsuariaByNombre(nombre);
         if (usuaria.isEmpty()) return new Usuaria("",0);
         return usuaria.get();
+    }
+
+    public Item cargaItem(String nombre) {
+        Optional<Item> item = this.repositoryItem.getItemByNombre(nombre);
+        if (item.isEmpty()) return new Item("", 0);
+        return item.get();
     }
 }
