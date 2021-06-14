25b7f048ca5e5c1f2478b6f1cd094d53c77f7bff
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 18:55:14 +0200

    feat(resourcesolli): implemented ordenar endpoint POST method at "/ordenar" endpoint

--- src/main/java/edu/pingpong/onequarkusapp/resource/ResourcesOlli.java
+++ src/main/java/edu/pingpong/onequarkusapp/resource/ResourcesOlli.java
@@ -1,9 +1,13 @@
 package edu.pingpong.onequarkusapp.resource;
 
+import edu.pingpong.onequarkusapp.entity.Item;
+import edu.pingpong.onequarkusapp.entity.Orden;
 import edu.pingpong.onequarkusapp.entity.Usuaria;
 import edu.pingpong.onequarkusapp.service.ServiceOlli;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,5 +37,16 @@ public class ResourcesOlli {
         Usuaria usuaria = service.cargaUsuaria(usuariaNombre);
         return !usuaria.getNombre().equals("") ? Response.ok(usuaria).build() :
                 Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    @Path("/ordena")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ordenar(/*@Valid*/ Orden orden) {
+        Orden newOrden = service.comanda(orden.getUser().getNombre(), orden.getItem().getNombre());
+        return newOrden != null ? Response.status(Response.Status.CREATED)
+                .entity(newOrden).build() : Response.status(Response.Status.NOT_FOUND).build();
     }
 }
