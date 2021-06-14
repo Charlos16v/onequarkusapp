85a8a3f5acc5821f9c2f61a992439b9c948d5e03
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 16:59:42 +0200

    feat(repositoryorden): implemented RepositoryOrden class the repository layer for Order Entity

---
+++ src/main/java/edu/pingpong/onequarkusapp/repository/RepositoryOrden.java
@@ -0,0 +1,15 @@
+package edu.pingpong.onequarkusapp.repository;
+
+import edu.pingpong.onequarkusapp.entity.Orden;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class RepositoryOrden implements PanacheRepository<Orden> {
+
+    public List<Orden> getOrdenesByNombre(String nombre) {
+        return this.list("ord_user", nombre);
+    }
+}
