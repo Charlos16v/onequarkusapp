7dd025435b4e2a030b255599029b661d6095865d
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-06-14 16:46:33 +0200

    feat(ServiceOlli): implemented cargaUser method encharged of return a Usuaria if it exists if not return a usuaria with default values

--- src/main/java/edu/pingpong/onequarkusapp/service/ServiceOlli.java
+++ src/main/java/edu/pingpong/onequarkusapp/service/ServiceOlli.java
@@ -1,16 +1,25 @@
 package edu.pingpong.onequarkusapp.service;
 
+import edu.pingpong.onequarkusapp.entity.Usuaria;
 import edu.pingpong.onequarkusapp.repository.RepositoryUsuaria;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Optional;
 
 // Single bean instance
 @ApplicationScoped
 public class ServiceOlli {
 
+    @Inject
+    RepositoryUsuaria repositoryUsuaria;
 
     public ServiceOlli() {}
 
 
+    public Usuaria cargaUsuaria(String nombre) {
+        Optional<Usuaria> usuaria = this.repositoryUsuaria.getUsuariaByNombre(nombre);
+        if (usuaria.isEmpty()) return new Usuaria("",0);
+        return usuaria.get();
+    }
 }
